abcd8d0f5699778b532ec40d98a86d369f7a08e8
greedsnake <42688909+greedsnake@users.noreply.github.com>
2018-11-12 15:02:03 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 24 18:12:45 2018
+
+@author: linzino
+"""
+
+from linebot.models import *
+from datetime import datetime 
+import feedparser
+import random
+# line-bot
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import *
+
+# mongodb
+from pymongo import MongoClient
+import pymongo
+import urllib.parse
+from datetime import datetime 
+
+# server-side
+from flask import Flask, request, abort
+
+# line-bot
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import *
+
+# package
+import re
+from datetime import datetime 
+
+# customer module
+import mongodb
+import corwler
+
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('l4pC2u/3DFbFW3pC0BE8huyEqMgaIZjlc0NzmqgLmm5+KXfRJLyjHIBwPPH5ize3LxjSKrbAUMi2hJEtJKKcRBdLg/MB6k/0g0n/dzZB+JImR7h8O1GRQaq3/JUNQaqxGhjTSWc0AnFsdIDIbcQ5LQdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('f44868616b14237cf0bd486d8e591e8e')
+
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+
+    
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(FollowEvent)
+def handle_follow(event):
+    '''
+    當使用者加入時觸動
+    '''
+    # 取得使用者資料
+    profile = line_bot_api.get_profile(event.source.user_id)
+    name = profile.display_name
+    uid = profile.user_id
+    
+    print(name)
+    print(uid)
+    # Udbddac07bac1811e17ffbbd9db459079
+    if mongodb.find_user(uid,'users')<= 0:
+        # 整理資料
+        dic = {'userid':uid,
+               'username':name,
+               'creattime':datetime.now(),
+               'Note':'user',
+               'ready':0}
+        
+        mongodb.insert_one(dic,'users')
+
+
+
+@handler.add(MessageEvent, message=TextMessage)
+    message1 = ""
+    message2 = ""
+    message3 = ""
+def handle_message(event):
+    '''
+    當收到使用者訊息的時候
+    '''
+    profile = line_bot_api.get_profile(event.source.user_id)
+    name = profile.display_name
+    uid = profile.user_id
+    message = event.message.text 
+           
+    def choose_id():
+      # 設定使用者下一句話要群廣播
+      mongodb.update_byid(uid,{'ready':1},'users')
+      message1 = 1
+      remessage = TextSendMessage(text='請輸入客戶編號')
+      line_bot_api.reply_message(
+                      event.reply_token,
+                      remessage)
+    def get_id(message):    
+      mongodb.update_byid(uid,{'ready':0},'users')
+      message1 = message
+      
+    def choose_age():
+      # 設定使用者下一句話要群廣播
+      mongodb.update_byid(uid,{'ready':1},'users')
+      message2 = 1
+      remessage = TextSendMessage(text='請輸入客戶年齡')
+      line_bot_api.reply_message(
+                      event.reply_token,
+                      remessage)
+      
+    def get_age(message):    
+      mongodb.update_byid(uid,{'ready':0},'users')
+      message2 = message
+    
+    def choose_gender():
+      # 設定使用者下一句話要群廣播
+      mongodb.update_byid(uid,{'ready':1},'users')
+      message3 = 1
+      remessage = TextSendMessage(text='請輸入客戶性別(0:女性; 1:男性')
+      line_bot_api.reply_message(
+                      event.reply_token,
+                      remessage)
+      
+    def get_gender(message):    
+      mongodb.update_byid(uid,{'ready':0},'users')
+      message3 = message
+      
+    def clear():
+      message1 = ""
+      message2 = ""
+      message3 = ""
+      remessage = TextSendMessage(text='已清除輸入資料')
+      line_bot_api.reply_message(
+                      event.reply_token,
+                      remessage)
+      
+    def suggest(message):
+      if message3==0:
+          sex="女性"
+      elif message3==1:
+          sex="男性"
+      else:
+          sex="未知"
+      remessage = TextSendMessage(text='查詢顧客ID=%s;年齡=%s; 性別=%s 的推薦商品:' %
+                                  (str(message1), str(message2), sex) )
+      line_bot_api.reply_message(
+                      event.reply_token,
+                      remessage)
+      text = report(message1,message2,message3)
+      # 包裝訊息
+      remessage = TextSendMessage(text=text)
+      # 回應使用者
+      line_bot_api.reply_message(
+                      event.reply_token,
+                      remessage)      
+    
+    def report(x,y,z):
+        
+        string = '推薦您使用:\n 1. %s\n 2. %s\n 3. %s\n 4. %s\n 5. %s\n' %
+            ('sony','小米','samsung','iphone','HTC')
+        return string
+      
+    if message == 'ID':
+        choose_id()
+        return 0 
+    
+    if message == '年齡':
+        choose_age()
+        return 0 
+    
+    if message == '性別':
+        choose_gender()
+        return 0 
+    
+    if message == '清除':
+        clear()
+        return 0 
+    
+    if message == '推薦':
+        suggest(message)
+        return 0 
+    
+    if mongodb.get_ready(uid,'users') ==1 & message==1:
+        get_id(message)
+        return 0 
+    
+    if mongodb.get_ready(uid,'users') ==1 & message2==1:
+        get_age(message)
+        return 0 
+    
+    if mongodb.get_ready(uid,'users') ==1 & message3==1:
+        get_gender(message)
+        return 0 
+    
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+    return 0 
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ mongodb.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 25 00:11:54 2018
+
+@author: linzino
+"""
+
+from pymongo import MongoClient
+import pymongo
+import urllib.parse
+from datetime import datetime 
+import pandas as pd
+
+
+# db setting
+host = 'ds133632.mlab.com'
+port = '33632'
+username = urllib.parse.quote_plus('greedsnake')
+password = urllib.parse.quote_plus('skane0605')
+# Authentication Database
+Authdb='snakedb'
+
+def init_db():
+    client = MongoClient('mongodb://%s:%s@%s:%s/%s?authMechanism=SCRAM-SHA-1'
+                      % (username, password, host, port, Authdb))
+    dbname='snakedb'
+    db = client[dbname]
+    return db
+
+
+def insert_one(dic,collection):
+    #collection_name = 'users'
+    db = init_db()
+    coll = db[collection]
+    coll.insert_one(dic)
+
+
+def get_all(collection):
+    db = init_db()
+    coll = db[collection]
+    return list(coll.find())
+
+def find_user(userid,collection):
+    '''
+    確認這個使用者是不是加入了
+    '''
+    db = init_db()
+    coll = db[collection]
+    return len(list(coll.find({"userid":userid})))
+
+def get_all_userid(collection):
+    db = init_db()
+    coll = db[collection]
+    unsers = list(coll.find())
+    
+    id_list = []
+    for user in unsers:
+        id_list.append(user['userid'])
+    
+    return id_list
+
+
+def get_ready(userid,collection):
+    db = init_db()
+    coll = db[collection]
+    unserinfo = list(coll.find({"userid":userid}))
+    return unserinfo[0]['ready']
+
+def update_byid(userid,setdict,collection):
+    db = init_db()
+    coll = db[collection]
+    coll.update({"userid":userid},{"$set":setdict}) 
+
